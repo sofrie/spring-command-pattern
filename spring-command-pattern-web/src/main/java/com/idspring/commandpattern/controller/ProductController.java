d1cef179420422da3364b54f939a0360600c013c
Chandra Ari Gunawan <chandra.gunawan@traveloka.com>
2020-10-02 10:32:07 +0700

    [Product] create get product detail API (#2)

--- spring-command-pattern-web/src/main/java/com/idspring/commandpattern/controller/ProductController.java
+++ spring-command-pattern-web/src/main/java/com/idspring/commandpattern/controller/ProductController.java
@@ -1,9 +1,16 @@
 package com.idspring.commandpattern.controller;
 
+import com.idspring.commandpattern.entity.Product;
+import com.idspring.commandpattern.model.controller.Response;
 import com.idspring.commandpattern.service.ServiceExecutor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 /**
  * @author chandraarigunawan
@@ -15,5 +22,9 @@ public class ProductController {
   @Autowired
   private ServiceExecutor serviceExecutor;
 
-  
+  @RequestMapping(value = "/{productId}", method = RequestMethod.GET,
+      produces = MediaType.APPLICATION_JSON_VALUE)
+  public Mono<Response<Product>> detail(@PathVariable("productId") String productId) {
+    throw new NotImplementedException();
+  }
 }
