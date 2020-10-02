b182fdccfa9b0901320edbe2770ebbcd8fba5159
sofrie <nagimagi47@gmail.com>
2020-10-02 10:55:36 +0700

    Update ProductController.java
    add get product by product id skeleton

--- spring-command-pattern-web/src/main/java/com/idspring/commandpattern/controller/ProductController.java
+++ spring-command-pattern-web/src/main/java/com/idspring/commandpattern/controller/ProductController.java
@@ -15,5 +15,10 @@ public class ProductController {
   @Autowired
   private ServiceExecutor serviceExecutor;
 
-  
+  @RequestMapping(value = "/{productId}", method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public Mono<Response<Product>> detail(@PathVariable("productId") String cartId) {
+        // get product by product id from product command
+        return null;
+    }
 }
